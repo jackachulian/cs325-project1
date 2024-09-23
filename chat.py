5fb3ea3204e9b4216c41747ee45153e94b7aa753
Jackachulian <jackachulian@gmail.com>
2024-09-23 12:16:28 -0500

    Implemented chat history

--- chat.py
+++ chat.py
@@ -15,27 +15,44 @@ ollama.create(model=model_name, modelfile=modelfile)
 with open('prompts.txt', 'r') as prompts_file:
     lines = prompts_file.readlines()
 
+messages = []
+
 # For each line, pass it to the LLM and stream the response message content
 with open("response.txt", "w") as response_file:
     for line in lines:
+
+        # Read the next line from the lines list.
         prompt = line.strip()
 
+        # Print it to screen, write to output file, and append to message history
         print("> "+prompt)
         response_file.write("> "+prompt+"\n")
+        messages.append({
+            'role': 'user', 
+            'content': prompt
+        })
 
+        # Start a stream and read from it to continuously print to the screen
         stream = ollama.chat(
             model=model_name,
-            messages=[{
-                'role': 'user', 
-                'content': prompt
-            }],
+            messages=messages,
             stream=True
         )
-
+  
+        # Continuously print the message, and store the response in a string by appending incoming content
+        response = ""
         for chunk in stream:
-            print(chunk['message']['content'], end='', flush=True)
-            response_file.write(chunk['message']['content'])
+            content = chunk['message']['content']
+            print(content, end='', flush=True)
+            response = response + content
 
+        # Write final reponse to the file and add a new message to history
+        response_file.write(response)
         response_file.write("\n\n")
+        messages.append({
+            'role': 'assistant', 
+            'content': response
+        })
+
         print()
         print()
